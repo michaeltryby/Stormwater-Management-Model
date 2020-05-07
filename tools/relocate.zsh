8d50e733371ffe41e1422196caf41838ca1dd68d
Michael Tryby <tryby.michael@epa.gov>
2020-05-07 17:55:05 -0400

    Makes lib relocation MacOS only

--- tools/relocate.zsh
+++ tools/relocate.zsh
@@ -1,15 +1,19 @@
 #!/usr/bin/env zsh
 
-# relocate.zsh - Relocate libomp to install/extern
 #
-# Arguments:
-#   1 - Path to library
+#  relocate.zsh - Relocate libomp to install/extern for MacOS
+#
+#  Date created: May 7, 2020
+#
+#  Author:       Michael E. Tryby
+#                US EPA - ORD/CESER
+#
+#  Arguments:
+#    1 - Path to library
 #
 
 
 echo "INFO: Relocating ... $1"
-
-#[[ ! -d extern ]] && { mkdir -v extern }
 
 
 chmod 755 extern/$1:t
@@ -20,6 +24,7 @@ IFS=$' '
 TOKEN=($( otool -l extern/$1:t | grep LC_ID_DYLIB -A2 | grep name ))
 LIB_PATH=${TOKEN[2]}
 
+
 # Changes load path for libomp in _solver.so
 install_name_tool -change ${LIB_PATH} @rpath/$1:t src/swmm/toolkit/_solver*.so
 
@@ -28,38 +33,3 @@ install_name_tool -change ${LIB_PATH} @rpath/$1:t lib/libswmm5.dylib
 
 # Changes id on relocated libomp
 install_name_tool -id @rpath/$1:t extern/$1:t
-
-
-# install
-#     extern
-#         libomp.dylib
-#             id
-#                 /usr/local/opt/libomp/lib/libomp.dylib
-#               * @rpath/libomp.dylib
-#             load
-#                 none
-#             rpath
-#                 none
-#
-#     lib
-#         libswmm5.dylib
-#             id
-#                 @rpath/libswmm5.dylib
-#             load
-#                 /usr/local/opt/libomp/lib/libomp.dylib
-#               * @rpath/libomp.dylib
-#             rpath
-#                 @loader_path;@loader_path/../.dylib
-#               * @loader_path/../extern
-#
-#     src/swmm/toolkit
-#         _solver.so
-#             id
-#                 none
-#             load
-#                 @rpath/libswmm5.dylib
-#                 /usr/local/opt/libomp/lib/libomp.dylib
-#               * @rpath/libomp.dylib
-#             rpath
-#                 @loader_path/../../../..
-#               * @loader_path/../../../..;@loader_path/../../../../extern
